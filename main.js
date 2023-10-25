e0142a01f8c40c17dfa85f1435a8fa2d8f0e9ea6
Simon Willison <swillison@gmail.com>
2023-10-24 19:49:16 -0700

    Add place markers from Overture maps
    See https://til.simonwillison.net/overture-maps/overture-maps-parquet#user-content-filtering-for-places-in-half-moon-bay

--- main.js
+++ main.js
@@ -25,4 +25,25 @@ const myMap = new maplibregl.Map({
 myMap.on("load", () => {
   const myBounds = myMap.getSource("protomaps").bounds;
   myMap.setMaxBounds(myBounds);
+  // Now load the places.json
+  fetch("places.json").then((response) => {
+    response.json().then((data) => {
+      data.rows.forEach((row) => {
+        const categories = JSON.parse(row.categories);
+        const catlist = [categories.main, ...(categories.alternate || [])].join(
+          ", ",
+        );
+        const name = JSON.parse(row.names).value[0][0].value[0];
+        const marker = new maplibregl.Marker({ scale: 0.5, color: "#000080" });
+        marker
+          .setLngLat([row.longitude, row.latitude])
+          .setPopup(
+            new maplibregl.Popup().setHTML(
+              `<strong>${name}</strong><br>${catlist}`,
+            ),
+          );
+        marker.addTo(myMap);
+      });
+    });
+  });
 });
